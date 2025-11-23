717e4a17a47ab51f8ae72a90714d5fdabc7c9561
arata1118 <arata@jigowatts.jp>
2025-11-23 14:53:21 +0900

    フッターを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import EmployeeList from "./components/EmployeeList";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
       <EmployeeList />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">Spring×React Application</footer>
+    </div>
+  );
+}
+
+export default Footer;
