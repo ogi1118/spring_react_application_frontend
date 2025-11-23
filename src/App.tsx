02b7c8affc85f0dce6bb047fbaa97011f80a75cf
arata1118 <arata@jigowatts.jp>
2025-11-23 14:42:45 +0900

    ヘッダーを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import EmployeeList from "./components/EmployeeList";
 
 function App() {
   return (
     <>
+      <Header />
       <EmployeeList />
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand">Employee Management System</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
