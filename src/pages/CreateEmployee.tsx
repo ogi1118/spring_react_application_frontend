e8b61b8420835c6fdf7a9d7639196c8c2acaa17c
arata1118 <arata@jigowatts.jp>
2025-11-23 20:17:24 +0900

    フォームを用意しコンソールに表示

--- src/pages/CreateEmployee.tsx
+++ src/pages/CreateEmployee.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateEmployeePage() {
-  return <div>CreateEmployeePage</div>;
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
+  const saveEmployee = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(firstName, lastName, email);
+    setEmail("");
+    setFirstName("");
+    setLastName("");
+  };
+
+  return (
+    <>
+      <div className="container">
+        <br/>
+        <br/>
+        <div className="row">
+          <div className="card col-md-6 offset-md-3">
+            <h2 className="text-center">Add Employee</h2>
+            <div className="card-body">
+              <form>
+                <div className="form-group mb-2">
+                  <label className="form-label">First Name</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Employee First Name"
+                    name="firstName"
+                    value={firstName}
+                    className="form-control"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <label className="form-label">Last Name</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Employee Last Name"
+                    name="lastName"
+                    value={lastName}
+                    className="form-control"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    placeholder="Enter Employee Email"
+                    name="email"
+                    value={email}
+                    className="form-control"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-success"
+                  onClick={saveEmployee}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateEmployeePage;
